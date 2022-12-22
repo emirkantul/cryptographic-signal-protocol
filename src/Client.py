7b936c8b45c2dfa1bbfd357ab552e8f9cddd175b
emirkantul <emirkantul@paratica.com>
2022-12-22 16:51:36 +0300

    Identity key generation and verification implemented

--- src/Client.py
+++ src/Client.py
@@ -1,35 +1,31 @@
 import math
 import time
-import random
 import sympy
 import warnings
-from random import randint, seed
 import sys
 from ecpy.curves import Curve,Point
 from Crypto.Hash import SHA3_256, HMAC, SHA256
 import requests
 from Crypto.Cipher import AES
-from Crypto import Random
+from Crypto.Random import random
 from Crypto.Util.Padding import pad
 from Crypto.Util.Padding import unpad
-import random
 import re
 import json
 
+
 API_URL = 'http://10.92.55.4:5000'
 
-stuID = 27041 #Enter Your ID
+stuID = 28129 #Enter Your ID
 
-#Server's Identitiy public key
-IKey_Ser = 1# Use the values in the project description document to form the server's IK as a point on the EC. Note that the values should be in decimal.
-
+# Given helper functions
 def IKRegReq(h,s,x,y):
     mes = {'ID':stuID, 'H': h, 'S': s, 'IKPUB.X': x, 'IKPUB.Y': y}
     print("Sending message is: ", mes)
     response = requests.put('{}/{}'.format(API_URL, "IKRegReq"), json = mes)		
     if((response.ok) == False): print(response.json())
 
-def IKRegVerify(code):
+def IKRegVerify(code, stuID, IKey_Pub):
     mes = {'ID':stuID, 'CODE': code}
     print("Sending message is: ", mes)
     response = requests.put('{}/{}'.format(API_URL, "IKRegVerif"), json = mes)
@@ -58,7 +54,6 @@ def OTKReg(keyID,x,y,hmac):
     if((response.ok) == False): return False
     else: return True
 
-
 def ResetIK(rcode):
     mes = {'ID':stuID, 'RCODE': rcode}
     print("Sending message is: ", mes)
@@ -75,11 +70,73 @@ def ResetSPK(h,s):
     if((response.ok) == False): return False
     else: return True
 
-
 def ResetOTK(h,s):
     mes = {'ID':stuID, 'H': h, 'S': s}
     print("Sending message is: ", mes)
     response = requests.delete('{}/{}'.format(API_URL, "ResetOTK"), json = mes)		
     if((response.ok) == False): print(response.json())
 
-print("deneme")
+# Get the curve, field, order and generator
+curve = Curve.get_curve('secp256k1')
+field = curve.field
+order = curve.order
+generator = curve.generator
+
+# Server's identity public key 
+IKey_Ser = Point(0xce1a69ecc226f9e667856ce37a44e50dbea3d58e3558078baee8fe5e017a556d , 0x13ddaf97158206b1d80258d7f6a6880e7aaf13180e060bb1e94174e419a4a093, curve)
+
+# Client's identity public key and private key
+IKey_Pub = Point(0xf061448e9f63ae9015605c705a2fe16da20270dac4518910fb66995cdd2b30c , 0xb363aae72aefeb60b50f692c980488d160140ca373d881347ac8e1c8150eb415, curve)
+IKey_Pr = 88811684008176007899207926081968991205376073643343601281773818664985783616780
+
+'''
+Identity Key generation
+
+# Select random secret sA where 0 < sA < n-1
+sA = Random.new().read(int(math.log(order - 1, 2)))
+sA = int.from_bytes(sA, byteorder = 'big') % order
+print("sA =",sA)
+
+# create long term public key
+Ik_pub = sA*generator
+print("Ik_pub =",Ik_pub)
+print("Ik_pub.x =",Ik_pub.x)
+print("Ik_pub.y =",Ik_pub.y)
+
+sA = 88811684008176007899207926081968991205376073643343601281773818664985783616780
+Ik_pub = (0xf061448e9f63ae9015605c705a2fe16da20270dac4518910fb66995cdd2b30c , 0xb363aae72aefeb60b50f692c980488d160140ca373d881347ac8e1c8150eb415)
+Ik_pub.x = 6795433811819819131752285391518736675983624010180123841332452236942390768396
+Ik_pub.y = 81140097284966191459956522116616668579410872729031413673803328453907451917333
+'''
+
+# Signature generator
+def generate_signature(stuID, IKey_Pr, generator, order): 
+    stuID_bytes = stuID.to_bytes(2,byteorder="big")
+    k = random.randint(1, order-1)
+    R = k * generator
+    r = (R.x) % order
+    r_bytes = r.to_bytes(32, 'big')
+    h = SHA3_256.SHA3_256_Hash(r_bytes+ stuID_bytes, True)
+    h = SHA3_256.SHA3_256_Hash.digest(h)
+    h = int.from_bytes(h,"big") % order
+    s = (k + (IKey_Pr*h)) % order
+
+    print("h =", h)
+    print("s =", s)
+    return h, s
+
+'''
+Registration signature generation 
+h, s = generate_signature(stuID=stuID, IKey_Pr=IKey_Pr, generator=generator, order=order)
+IKRegReq(h, s, IKey_Pub.x, IKey_Pub.y)
+
+code = 380271
+'''
+
+code = 380271
+'''
+Registration verification 
+IKRegVerify(code=code, stuID=stuID, IKey_Pub=IKey_Pub)
+
+rcode = 765639
+'''
