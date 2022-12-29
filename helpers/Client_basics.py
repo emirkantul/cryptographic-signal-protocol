de8e83de939e2a959233d909999ab229420c7a85
emirkantul <emirkantul@paratica.com>
2022-12-29 22:16:18 +0300

    phase2 initial

---
+++ helpers/Client_basics.py
@@ -0,0 +1,164 @@
+import math
+import time
+import random
+import sympy
+import warnings
+from random import randint, seed
+import sys
+from ecpy.curves import Curve,Point
+from Crypto.Hash import SHA3_256, HMAC, SHA256
+import requests
+from Crypto.Cipher import AES
+from Crypto import Random
+from Crypto.Util.Padding import pad
+from Crypto.Util.Padding import unpad
+import random
+import re
+import json
+
+API_URL = 'http://10.92.52.255:5000/'
+
+stuID = 26045
+stuIDB = 2014
+
+def egcd(a, b):
+    x,y, u,v = 0,1, 1,0
+    while a != 0:
+        q, r = b//a, b%a
+        m, n = x-u*q, y-v*q
+        b,a, x,y, u,v = a,r, u,v, m,n
+    gcd = b
+    return gcd, x, y
+
+def modinv(a, m):
+    gcd, x, y = egcd(a, m)
+    if gcd != 1:
+        return None  # modular inverse does not exist
+    else:
+        return x % m
+
+def Setup():
+    E = Curve.get_curve('secp256k1')
+    return E
+
+def KeyGen(E):
+    n = E.order
+    P = E.generator
+    sA = randint(1,n-1)
+    QA = sA*P
+    return sA, QA
+
+def SignGen(message, E, sA):
+    n = E.order
+    P = E.generator
+    k = randint(1, n-2)
+    R = k*P
+    r = R.x % n
+    h = int.from_bytes(SHA3_256.new(r.to_bytes((r.bit_length()+7)//8, byteorder='big')+message).digest(), byteorder='big')%n
+    s = (sA*h + k) % n
+    return h, s
+
+def SignVer(message, h, s, E, QA):
+    n = E.order
+    P = E.generator
+    V = s*P - h*QA
+    v = V.x % n
+    h_ = int.from_bytes(SHA3_256.new(v.to_bytes((v.bit_length()+7)//8, byteorder='big')+message).digest(), byteorder='big')%n
+    if h_ == h:
+        return True
+    else:
+        return False
+
+
+#server's Identitiy public key
+IKey_Ser = Point(93223115898197558905062012489877327981787036929201444813217704012422483432813 , 8985629203225767185464920094198364255740987346743912071843303975587695337619, curve)
+
+def IKRegReq(h,s,x,y):
+    mes = {'ID':stuID, 'H': h, 'S': s, 'IKPUB.X': x, 'IKPUB.Y': y}
+    print("Sending message is: ", mes)
+    response = requests.put('{}/{}'.format(API_URL, "IKRegReq"), json = mes)		
+    if((response.ok) == False): print(response.json())
+
+def IKRegVerify(code):
+    mes = {'ID':stuID, 'CODE': code}
+    print("Sending message is: ", mes)
+    response = requests.put('{}/{}'.format(API_URL, "IKRegVerif"), json = mes)
+    if((response.ok) == False): raise Exception(response.json())
+    print(response.json())
+
+def SPKReg(h,s,x,y):
+    mes = {'ID':stuID, 'H': h, 'S': s, 'SPKPUB.X': x, 'SPKPUB.Y': y}
+    print("Sending message is: ", mes)
+    response = requests.put('{}/{}'.format(API_URL, "SPKReg"), json = mes)		
+    if((response.ok) == False): 
+        print(response.json())
+    else: 
+        res = response.json()
+        return res['SPKPUB.X'], res['SPKPUB.Y'], res['H'], res['S']
+
+def OTKReg(keyID,x,y,hmac):
+    mes = {'ID':stuID, 'KEYID': keyID, 'OTKI.X': x, 'OTKI.Y': y, 'HMACI': hmac}
+    print("Sending message is: ", mes)
+    response = requests.put('{}/{}'.format(API_URL, "OTKReg"), json = mes)		
+    print(response.json())
+    if((response.ok) == False): return False
+    else: return True
+
+
+def ResetIK(rcode):
+    mes = {'ID':stuID, 'RCODE': rcode}
+    print("Sending message is: ", mes)
+    response = requests.delete('{}/{}'.format(API_URL, "ResetIK"), json = mes)		
+    print(response.json())
+    if((response.ok) == False): return False
+    else: return True
+
+def ResetSPK(h,s):
+    mes = {'ID':stuID, 'H': h, 'S': s}
+    print("Sending message is: ", mes)
+    response = requests.delete('{}/{}'.format(API_URL, "ResetSPK"), json = mes)		
+    print(response.json())
+    if((response.ok) == False): return False
+    else: return True
+
+def ResetOTK(h,s):
+    mes = {'ID':stuID, 'H': h, 'S': s}
+    print("Sending message is: ", mes)
+    response = requests.delete('{}/{}'.format(API_URL, "ResetOTK"), json = mes)		
+    print(response.json())
+
+############## The new functions of phase 2 ###############
+
+#Pseudo-client will send you 5 messages to your inbox via server when you call this function
+def PseudoSendMsg(h,s):
+    mes = {'ID':stuID, 'H': h, 'S': s}
+    print("Sending message is: ", mes)
+    response = requests.put('{}/{}'.format(API_URL, "PseudoSendMsg"), json = mes)		
+    print(response.json())
+
+#Get your messages. server will send 1 message from your inbox
+def ReqMsg(h,s):
+    mes = {'ID':stuID, 'H': h, 'S': s}
+    print("Sending message is: ", mes)
+    response = requests.get('{}/{}'.format(API_URL, "ReqMsg"), json = mes)	
+    print(response.json())	
+    if((response.ok) == True): 
+        res = response.json()
+        return res["IDB"], res["OTKID"], res["MSGID"], res["MSG"], res["EK.X"], res["EK.Y"]
+
+#Get the list of the deleted messages' ids.
+def ReqDelMsg(h,s):
+    mes = {'ID':stuID, 'H': h, 'S': s}
+    print("Sending message is: ", mes)
+    response = requests.get('{}/{}'.format(API_URL, "ReqDelMsgs"), json = mes)      
+    print(response.json())      
+    if((response.ok) == True): 
+        res = response.json()
+        return res["MSGID"]
+
+#If you decrypted the message, send back the plaintext for checking
+def Checker(stuID, stuIDB, msgID, decmsg):
+    mes = {'IDA':stuID, 'IDB':stuIDB, 'MSGID': msgID, 'DECMSG': decmsg}
+    print("Sending message is: ", mes)
+    response = requests.put('{}/{}'.format(API_URL, "Checker"), json = mes)		
+    print(response.json())
--- src/Client.py
+++ src/Client.py
@@ -1,25 +1,18 @@
 # Melike Soyturk 28129
 # Emir Kantul 27041
-# CS411 Project Phase I
+# CS411 Project Phase II
 
+from random import randint
 from ecpy.curves import Curve,Point
 from Crypto.Hash import SHA3_256, SHA256
 import requests
 from Crypto.Random import random
 from Crypto.Hash import  HMAC
-
-# NOTE: 
-# Parts for key generation and registration are commented to 
-# prevent repeated key generation and request to servers. After
-# getting appropriate results, static variables used and rest
-# is commented. For full functionality and test those sections
-# should be uncommented.
     
-# Also all methods were tried with the other member's student id as well
+API_URL = 'http://10.92.52.255:5000/'
 
-API_URL = 'http://10.92.55.4:5000'
-
-stuID = 27041 #Enter Your ID
+stuIDB = 2014
+stuID = 27041
 
 # Given helper functions
 def IKRegReq(h,s,x,y):
@@ -145,24 +138,15 @@ print("\nClient Public Identity Key:", IKey_Pub)
 print("Client Private Identity Key:", IKey_Pr)
 
 '''
-# Identity Key generation
 IKey_Pr, IKey_Pub = generate_private_key(order, generator)
-
-# IKey_Pr: 54711695610845891711285415678093810504562500554097973349097378295447174968645
-# IKey_Pub: (0xeaa0b601668eba6177eb13991f611a8a3017fe64ca9635fc2b8154d3f78e1954 , 0x576cc7f8888c1291a324704a695539cf8e4f86634a4527b69cc27dcb4109c4240x576cc7f8888c1291a324704a695539cf8e4f86634a4527b69cc27dcb4109c424)
-# IKey_Pub.x: 106125158254444507586124357737838011183233385576686272071348298652513412716884
-# IKey_Pub.y: 39543417457710120237121373054221395027999800633142105468250786347587046327332
 '''
 
 
 '''
 IK Registration signature generation 
-
 stuID_bytes = stuID.to_bytes((stuID.bit_length() +7)//8,byteorder="big")
 h, s = generate_signature(msg=stuID_bytes, sA=IKey_Pr, generator=generator, order=order)
 IKRegReq(h, s, IKey_Pub.x, IKey_Pub.y)
-
-code = 103096
 '''
 code = 103096
 
@@ -181,44 +165,31 @@ print("Signed Pre-key Public:",SPKey_Pub)
 
 '''
 # Signed Pre-key generation
-
 SPKey_Pr, SPKey_pub = generate_private_key(order, generator)
-
-# SPKey_Pr: 24373054577699454663564151012111011615866473942778748787713915925292914841793
-# SPKey_Pub: (0xc763f5b6ca6fc8fb746d204b99fd47df2a79056d668dc613894c55bdff47398d , 0x5b7cf45b7ee2d189189fa38d47a6cbb2b986bf539620b2695840711bc32c9333)
-# SPKey_Pub.x:  90186870583367397078994984968597413955227984672863262182290021294247000881549
-# SPKey_Pub.y: 41381244749936181075457993058941167828970061873112217764976555947988353848115
 '''
 
 
 '''
-# SPK Registration signature generation 
-
+# SPK Registration signature generation
 msg = SPKey_Pub.x.to_bytes((SPKey_Pub.x.bit_length()+7)//8,byteorder="big") + SPKey_Pub.y.to_bytes((SPKey_Pub.y.bit_length()+7)//8,byteorder="big")
 h, s = generate_signature(msg=msg, sA=IKey_Pr, generator=generator, order=order)
 resp_x, resp_y, resp_h, resp_s  = SPKReg(h, s, SPKey_Pub.x, SPKey_Pub.y)
 
-# Server Point: (0x7d38f788a09a94b29ef81b95e812816889e9d2fcbbd51909c94cbda2e9bcc736 , 0x8585933fcc79add8ab5bb6a824e5170b68d82c12a543b1576610dd4210775333)
-'''
-
-
-'''
 # Signature Verification
-
 resp_x_bytes = resp_x.to_bytes((resp_x.bit_length() + 7) // 8, byteorder='big')
 resp_y_bytes = resp_y.to_bytes((resp_y.bit_length() + 7) // 8, byteorder='big')
 resp_h_bytes = resp_h.to_bytes((resp_h.bit_length() + 7) // 8, byteorder='big')
 resp_s_bytes = resp_s.to_bytes((resp_s.bit_length() + 7) // 8, byteorder='big')
 resp_msg = resp_x_bytes + resp_y_bytes
 SPK_Pub_Server = Point(resp_x, resp_y, curve)
-print("Server Point:", SPK_Pub_Server)
-
 msg = SPKey_Pub.x.to_bytes((resp_x.bit_length()+7)//8,byteorder="big") + resp_y.to_bytes((SPKey_Pub.y.bit_length()+7)//8,byteorder="big")
 verify_signature(h=resp_h, s=resp_s, qA=IKey_Ser, msg=resp_msg, order=order, generator=generator)
 msg = SPKey_Pub.x.to_bytes((resp_x.bit_length()+7)//8,byteorder="big") + resp_y.to_bytes((SPKey_Pub.y.bit_length()+7)//8,byteorder="big")
 verify_signature(resp_h, resp_s, SPKey_Pub, msg, order, generator)
 '''
+
 SPK_Pub_Server = Point(0x7d38f788a09a94b29ef81b95e812816889e9d2fcbbd51909c94cbda2e9bcc736 , 0x8585933fcc79add8ab5bb6a824e5170b68d82c12a543b1576610dd4210775333, curve=curve)
+print("Server Point:", SPK_Pub_Server)
 
 
 # Generating HMAC Key
@@ -245,3 +216,193 @@ for i in range(10):
     temp = OTK0_x_bytes + OTK0_y_bytes
     hmac0 = HMAC.new(key=k_HMAC_bytes, msg=temp, digestmod=SHA256)
     OTKReg(i, OTK0_pub.x, OTK0_pub.y, hmac0.hexdigest())
+
+#####################################################################################
+###################################### PHASE 2 ######################################
+#####################################################################################
+
+# phase 2 helpers
+
+API_URL = 'http://10.92.52.255:5000/'
+
+stuID = 26045
+stuIDB = 2014
+
+def egcd(a, b):
+    x,y, u,v = 0,1, 1,0
+    while a != 0:
+        q, r = b//a, b%a
+        m, n = x-u*q, y-v*q
+        b,a, x,y, u,v = a,r, u,v, m,n
+    gcd = b
+    return gcd, x, y
+
+def modinv(a, m):
+    gcd, x, y = egcd(a, m)
+    if gcd != 1:
+        return None  # modular inverse does not exist
+    else:
+        return x % m
+
+def Setup():
+    E = Curve.get_curve('secp256k1')
+    return E
+
+def KeyGen(E):
+    n = E.order
+    P = E.generator
+    sA = randint(1,n-1)
+    QA = sA*P
+    return sA, QA
+
+def SignGen(message, E, sA):
+    n = E.order
+    P = E.generator
+    k = randint(1, n-2)
+    R = k*P
+    r = R.x % n
+    h = int.from_bytes(SHA3_256.new(r.to_bytes((r.bit_length()+7)//8, byteorder='big')+message).digest(), byteorder='big')%n
+    s = (sA*h + k) % n
+    return h, s
+
+def SignVer(message, h, s, E, QA):
+    n = E.order
+    P = E.generator
+    V = s*P - h*QA
+    v = V.x % n
+    h_ = int.from_bytes(SHA3_256.new(v.to_bytes((v.bit_length()+7)//8, byteorder='big')+message).digest(), byteorder='big')%n
+    if h_ == h:
+        return True
+    else:
+        return False
+
+#Pseudo-client will send you 5 messages to your inbox via server when you call this function
+def PseudoSendMsg(h,s):
+    mes = {'ID':stuID, 'H': h, 'S': s}
+    print("Sending message is: ", mes)
+    response = requests.put('{}/{}'.format(API_URL, "PseudoSendMsg"), json = mes)		
+    print(response.json())
+
+#Get your messages. server will send 1 message from your inbox
+def ReqMsg(h,s):
+    mes = {'ID':stuID, 'H': h, 'S': s}
+    print("Sending message is: ", mes)
+    response = requests.get('{}/{}'.format(API_URL, "ReqMsg"), json = mes)	
+    print(response.json())	
+    if((response.ok) == True): 
+        res = response.json()
+        return res["IDB"], res["OTKID"], res["MSGID"], res["MSG"], res["EK.X"], res["EK.Y"]
+
+#Get the list of the deleted messages' ids.
+def ReqDelMsg(h,s):
+    mes = {'ID':stuID, 'H': h, 'S': s}
+    print("Sending message is: ", mes)
+    response = requests.get('{}/{}'.format(API_URL, "ReqDelMsgs"), json = mes)      
+    print(response.json())      
+    if((response.ok) == True): 
+        res = response.json()
+        return res["MSGID"]
+
+#If you decrypted the message, send back the plaintext for checking
+def Checker(stuID, stuIDB, msgID, decmsg):
+    mes = {'IDA':stuID, 'IDB':stuIDB, 'MSGID': msgID, 'DECMSG': decmsg}
+    print("Sending message is: ", mes)
+    response = requests.put('{}/{}'.format(API_URL, "Checker"), json = mes)	
+
+
+# OTK data
+otk_private = [81661962775791531255393590218032810348611196057800092950972910673728483913293, 52808665550935137250118206376573576203141304345939680267257950392200540207015, 28886528209940086927211574720079351376569346129303597582466848064924258131196, 11114466964466141026367702073664155613887604275985718533928007399121172719121, 17166357728138738026932544999677587823688912721806684576840951659670965088038, 89828764614649288349946443494126545878918699713795459712736909795019648787538, 48168878674606644491705808962129225172098876787988203995044137678806445123558, 91179985255603002478337425498383773831413918717804015885447630247048388959701, 82924893139980026608916710627158509866282852529554964410349276866878225755082, 90870193415124165058039276172179735704530307717037153962822467285881689168350]
+
+# Signing my stuID with my private IK in order to get messages from the server:
+k = random.randint(1, order - 2)
+R = k * generator
+r = R.x % order
+
+r_byte_array = r.to_bytes((r.bit_length() +7)//8, byteorder = 'big')
+m_byte_array = stuID.to_bytes((stuID.bit_length() +7)//8, byteorder = 'big')
+r_m = r_byte_array + m_byte_array #concatenation of r and m where m is stuID
+
+h = SHA3_256.new(r_m)
+h = int.from_bytes(h.digest(), byteorder='big') % order
+
+s = (k - (IKey_Pr * h)) % order
+print("s:", s)
+print("h:", h)
+
+PseudoSendMsg(h,s)
+
+# requesting messages from the server:
+otkID_array = []
+msgID_array = []
+msg_array = []
+for i in range(5):
+    stuIDB, otkID, msgID, msg, ek_x, ek_y = ReqMsg(h,s)
+    msg_array.append(msg)
+    otkID_array.append(otkID)
+    msgID_array.append(msgID)
+
+first_iteration = True
+counter = 0
+for message in msg_array:
+    message_byte_array = message.to_bytes((message.bit_length() +7)//8, byteorder = 'big')
+    
+    # hmac of the message
+    message_HMAC = message_byte_array[len(message_byte_array)-32:]
+    #print(message_HMAC) 
+
+    # nonce and the ciphertext
+    message_with_nonce = message_byte_array[:len(message_byte_array)-32]
+    #print(message_with_nonce)
+
+    # ciphertext
+    ciphertext = message_byte_array[8:len(message_byte_array)-32]
+    #print(ciphertext)
+
+
+    # _session key generation start_
+    EK_B_point = Point(ek_x, ek_y, curve = curve)
+    T = otk_private[otkID] * EK_B_point
+    T_x = T.x
+    T_y = T.y
+
+    T_x_byte_array = T_x.to_bytes((T_x.bit_length() +7)//8, byteorder = 'big')
+    T_y_byte_array = T_y.to_bytes((T_y.bit_length() +7)//8, byteorder = 'big')
+    U = T_x_byte_array + T_y_byte_array + b'ToBeOrNotToBe'
+
+    K_S = SHA3_256.new(U)
+    # _session key generation end_
+
+    # _key derivation start_
+    if(first_iteration):
+        K_KDF = K_S
+        first_iteration = False
+    else:
+        K_KDF = SHA3_256.new(K_ENC.digest() + K_HMAC.digest() + b'MayTheForceBeWithYou')
+
+    K_ENC = SHA3_256.new(K_KDF.digest() + b'YouTalkingToMe')
+    K_HMAC = SHA3_256.new(K_KDF.digest() + K_ENC.digest() + b'YouCannotHandleTheTruth')
+    # _key derivation end_
+
+    # obtaining hmac from ciphertext
+    hmac = HMAC.new(K_HMAC.digest(), ciphertext, digestmod=SHA256)
+    hmac = hmac.digest()
+
+    # checking MAC values
+    if(hmac == message_HMAC):
+        print("HMAC is verified")
+
+        # dencryption of the message
+        cipher = AES.new(K_ENC.digest(), AES.MODE_CTR, nonce = message_with_nonce[0:8])
+        dtext = cipher.decrypt(message_with_nonce[8:])
+        decrypted_message = dtext.decode('utf-8')
+
+        print("Decrypted message:", decrypted_message)
+
+        # cheking decrypted message
+        Checker(stuID, stuIDB, msgID_array[counter], decrypted_message)
+    else: 
+        print("HMAC is not verified")
+        Checker(stuID, stuIDB, msgID_array[counter], 'INVALIDHMAC')
+
+    counter = counter + 1
+
